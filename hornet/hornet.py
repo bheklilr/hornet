d53c53005e3a49e40c0bee011d864eac952c899b
Aaron Stevens <bheklilr2@gmail.com>
2017-05-18 10:27:09 -0500

    Starting to fill out basic implementation of the mapping structure, with tests

--- hornet/hornet.py
+++ hornet/hornet.py
@@ -1,21 +1,31 @@
-from collections import MutableMapping
+from collections import Mapping, MutableMapping
 
 
 class Hornet(MutableMapping):
-    def __init__(self, items=(), *, object_pairs_hook=dict):
-        pass
+    @classmethod
+    def object_pairs_hook(cls):
+        return dict
+
+    def __init__(self, items=(), **kwargs):
+        self._underlying_mapping = self.object_pairs_hook()()
+        if isinstance(items, Mapping):
+            items = items.items()
+        for k, v in items:
+            self._underlying_mapping[k] = v
+        for k, v in kwargs.items():
+            self._underlying_mapping[k] = v
 
     def __getitem__(self, key):
-        raise NotImplementedError()
+        return self._underlying_mapping[key]
 
     def __setitem__(self, key, value):
-        raise NotImplementedError()
+        self._underlying_mapping[key] = value
 
-    def __delitem__(self, key, value):
-        raise NotImplementedError()
+    def __delitem__(self, key):
+        del self._underlying_mapping[key]
 
     def __iter__(self):
-        raise NotImplementedError()
+        return iter(self._underlying_mapping)
 
     def __len__(self):
-        raise NotImplementedError()
+        return len(self._underlying_mapping)
